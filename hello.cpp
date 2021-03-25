2811abc8bb423324869e6adb91a79d3c95205e87
Colbyn Wadman <hello@colbyn.com>
2021-03-25 12:10:14 -0600

    wip

--- hello.cpp
+++ hello.cpp
@@ -10,7 +10,68 @@
 #include <symengine/symengine_exception.h>
 #include <symengine/visitor.h>
 #include <symengine/solve.h>
+#include <symengine/polys/cancel.h>
+#include <symengine/polys/uintpoly_flint.h>
+#include <symengine/polys/upolybase.h>
+#include <symengine/polys/uratpoly.h>
+#include <symengine/polys/basic_conversions.h>
+#include <symengine/polys/msymenginepoly.h>
+#include <symengine/polys/uexprpoly.h>
+#include <symengine/polys/uintpoly.h>
+#include <symengine/polys/uratpoly.h>
+
 using namespace std;
+
+using SymEngine::Expression;
+using SymEngine::symbol;
+using SymEngine::eq;
+using SymEngine::integer;
+using SymEngine::real_double;
+using SymEngine::complex_double;
+using SymEngine::sin;
+using SymEngine::cos;
+using SymEngine::pi;
+using SymEngine::print_stack_on_segfault;
+using SymEngine::RCP;
+using SymEngine::rational;
+using SymEngine::DenseMatrix;
+using SymEngine::Basic;
+using SymEngine::Complex;
+using SymEngine::Symbol;
+using SymEngine::is_a;
+using SymEngine::set_basic;
+using SymEngine::Add;
+using SymEngine::minus_one;
+using SymEngine::CSRMatrix;
+using SymEngine::add;
+using SymEngine::sub;
+using SymEngine::eye;
+using SymEngine::diag;
+using SymEngine::vec_basic;
+using SymEngine::function_symbol;
+using SymEngine::permutelist;
+using SymEngine::SymEngineException;
+using SymEngine::eigen_values;
+using SymEngine::finiteset;
+using SymEngine::one;
+using SymEngine::mul;
+using SymEngine::down_cast;
+using SymEngine::RealDouble;
+using SymEngine::conjugate;
+using SymEngine::cancel;
+using SymEngine::Basic;
+using SymEngine::RCP;
+using SymEngine::exp;
+using SymEngine::integer;
+using SymEngine::mul;
+using SymEngine::pow;
+using SymEngine::sub;
+using SymEngine::symbol;
+using SymEngine::UIntPoly;
+using SymEngine::integer_class;
+using SymEngine::vec_integer_class;
+using SymEngine::outArg;
+using namespace SymEngine::literals;
 
 // __cxa_rethrow
 // __cxa_end_catch
@@ -26,52 +87,28 @@ using namespace std;
 // void __cxa_throw(void* thrown_exception, void* tinfo, void (*dest)(void*)) {abort();}
 
 int main() {
-    using SymEngine::Expression;
-    using SymEngine::symbol;
-    using SymEngine::eq;
-    using SymEngine::integer;
-    using SymEngine::real_double;
-    using SymEngine::complex_double;
-    using SymEngine::sin;
-    using SymEngine::cos;
-    using SymEngine::pi;
-    using SymEngine::print_stack_on_segfault;
-    using SymEngine::RCP;
-    using SymEngine::rational;
-    using SymEngine::DenseMatrix;
-    using SymEngine::Basic;
-    using SymEngine::Complex;
-    using SymEngine::Symbol;
-    using SymEngine::is_a;
-    using SymEngine::set_basic;
-    using SymEngine::Add;
-    using SymEngine::minus_one;
-    using SymEngine::CSRMatrix;
-    using SymEngine::add;
-    using SymEngine::sub;
-    using SymEngine::eye;
-    using SymEngine::diag;
-    using SymEngine::vec_basic;
-    using SymEngine::function_symbol;
-    using SymEngine::permutelist;
-    using SymEngine::SymEngineException;
-    using SymEngine::eigen_values;
-    using SymEngine::finiteset;
-    using SymEngine::one;
-    using SymEngine::mul;
-    using SymEngine::down_cast;
-    using SymEngine::RealDouble;
-    using SymEngine::conjugate;
+    RCP<const Basic> x = symbol("x");
+    RCP<const Basic> y = symbol("y");
+    RCP<const UIntPoly> numer, denom, common;
 
-    Expression e0("x + 1");
+    // 2*x / x = 2 / 1
+    SymEngine::cancel(mul(x, integer(2)), x, outArg(numer), outArg(denom), outArg(common));
+    // REQUIRE(numer->__str__() == "2");
+    // REQUIRE(denom->__str__() == "1");
+    // REQUIRE(common->__str__() == "x");
+    // std::cout << numer->__str__() << std::endl;
+    // std::cout << denom->__str__() << std::endl;
+    // std::cout << common->__str__() << std::endl;
 
-    vec_basic elems{
-        integer(1), integer(0),
-        integer(-1), integer(-2)
-    };
-    DenseMatrix A = DenseMatrix(2, 2, elems);
+    // std::cout << e0 << std::endl;
 
-    std::cout << "value:\n" << A << std::endl;
-    printf("HELLO WORLD\n");
+    // vec_basic elems{
+    //     integer(1), integer(0),
+    //     integer(-1), integer(-2)
+    // };
+    // DenseMatrix A = DenseMatrix(2, 2, elems);
+
+    // std::cout << A << std::endl;
+    printf("\nDONE\n");
     return 0;
 }
